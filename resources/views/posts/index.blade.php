ea6e10978fde3931ddbf6ec8e357216e655ac6d0
Victor <victorzevallos@protonmail.com>
2020-11-18 17:38:59 -0500

    Add posts form

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,7 +3,32 @@
 @section('content')
     <div class="flex justify-center">
         <div class="w-8/12 bg-white p-6 rounded-lg">
-            Post
+            <form action="{{ route('posts') }}" method="post">
+                @csrf
+                <div class="mb-4">
+                    <label class="sr-only" for="body">Body</label>
+                    <textarea
+                        id="body"
+                        name="body"
+                        cols="30"
+                        rows="4"
+                        placeholder="Post something!"
+                        class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('body') border-red-500 @enderror"
+                    ></textarea>
+
+                    @error('body')
+                        <div class="text-red-500 mt-2 text-sm">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+
+                <div>
+                    <button class="bg-blue-500 text-white px-4 py-2 rounded font-medium">
+                        Post
+                    </button>
+                </div>
+            </form>
         </div>
     </div>
 @endsection
