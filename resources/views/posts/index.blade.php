30e1a7f0cf75ef6ee0551e2f6b75b717a60eb3a7
Victor <victorzevallos@protonmail.com>
2020-11-19 15:48:30 -0500

    Add middleware

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,31 +3,34 @@
 @section('content')
     <div class="flex justify-center">
         <div class="w-8/12 bg-white p-6 rounded-lg">
-            <form action="{{ route('posts') }}" method="post" class="mb-4">
-                @csrf <div class="mb-4">
-                    <label class="sr-only" for="body">Body</label>
-                    <textarea
-                        id="body"
-                        name="body"
-                        cols="30"
-                        rows="4"
-                        placeholder="Post something!"
-                        class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('body') border-red-500 @enderror"
-                    ></textarea>
+            @auth
+                <form action="{{ route('posts') }}" method="post" class="mb-4">
+                    @csrf <div class="mb-4">
+                        <label class="sr-only" for="body">Body</label>
+                        <textarea
+                            id="body"
+                            name="body"
+                            cols="30"
+                            rows="4"
+                            placeholder="Post something!"
+                            class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('body') border-red-500 @enderror"
+                            ></textarea>
 
-                    @error('body')
-                        <div class="text-red-500 mt-2 text-sm">
-                            {{ $message }}
-                        </div>
-                    @enderror
-                </div>
+                        @error('body')
+                            <div class="text-red-500 mt-2 text-sm">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
 
-                <div>
-                    <button class="bg-blue-500 text-white px-4 py-2 rounded font-medium">
-                        Post
-                    </button>
-                </div>
-            </form>
+                    <div>
+                        <button class="bg-blue-500 text-white px-4 py-2 rounded font-medium">
+                            Post
+                        </button>
+                    </div>
+                </form>
+            @endauth
+
 
             @if ($posts->count())
                 @foreach ($posts as $post)
