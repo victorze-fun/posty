4fb7e02512f5f2c7e7be83b3556ea58450581f53
Victor <victorzevallos@protonmail.com>
2020-11-19 10:29:16 -0500

    Add authorization

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -37,13 +37,13 @@
 
                         <p class="mb-2">{{ $post->body }}</p>
 
-                        <div>
+                        @can('delete', $post)
                             <form action="{{ route('posts.destroy', $post) }}" method="post">
                                 @csrf
                                 @method('DELETE')
                                 <button type="submit" class="text-blue-500">Delete</button>
                             </form>
-                        </div>
+                        @endcan
 
                         <div class="flex items-center">
                             @auth
