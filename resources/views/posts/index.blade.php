c92d04ed78f31b534df0415bdd84f390c669b216
Victor <victorzevallos@protonmail.com>
2020-11-18 19:52:46 -0500

    Add pagination

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -39,6 +39,8 @@
                         <p class="mb-2">{{ $post->body }}</p>
                     </div>
                 @endforeach
+
+                {{ $posts->links() }}
             @else
                 <p>There are no posts</p>
             @endif
