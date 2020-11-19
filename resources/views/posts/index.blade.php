cbcbc777c5bf96b14332ec919372fe49967cfc79
Victor <victorzevallos@protonmail.com>
2020-11-18 21:23:37 -0500

    Add like post

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -37,6 +37,22 @@
                         <span class="text-gray-600">{{ $post->created_at->diffForHumans() }}</span>
 
                         <p class="mb-2">{{ $post->body }}</p>
+
+                        <div class="flex items-center">
+                            @if (!$post->likedBy(auth()->user()))
+                                <form action="{{ route('posts.likes', $post->id) }}" method="post" class="mr-1">
+                                    @csrf
+                                    <button type="submit" class="text-blue-500">Like</button>
+                                </form>
+                            @else
+                                <form action="" method="post" class="mr-1">
+                                    @csrf
+                                    <button type="submit" class="text-blue-500">Unlike</button>
+                                </form>
+                            @endif
+
+                            <span>{{ $post->likes->count() }} {{ Str::plural('like', $post->likes->count()) }}</span>
+                        </div>
                     </div>
                 @endforeach
 
