f19a5b4c876f3a9bbba7f8e91b049b5975e92457
Victor <victorzevallos@protonmail.com>
2020-11-18 19:14:23 -0500

    Add post list

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="flex justify-center">
         <div class="w-8/12 bg-white p-6 rounded-lg">
-            <form action="{{ route('posts') }}" method="post">
+            <form action="{{ route('posts') }}" method="post" class="mb-4">
                 @csrf
                 <div class="mb-4">
                     <label class="sr-only" for="body">Body</label>
@@ -29,6 +29,19 @@
                     </button>
                 </div>
             </form>
+
+            @if ($posts->count())
+                @foreach ($posts as $post)
+                    <div class="mb-4">
+                        <a href="" class="font-bold">{{ $post->user->name }}</a>
+                        <span class="text-gray-600">{{ $post->created_at->diffForHumans() }}</span>
+
+                        <p class="mb-2">{{ $post->body }}</p>
+                    </div>
+                @endforeach
+            @else
+                <p>There are no posts</p>
+            @endif
         </div>
     </div>
 @endsection
